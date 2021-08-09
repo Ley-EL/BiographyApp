fdcd9c82350bd9af7ef1831579ea93ff5f6fca47
Ley-EL <bornokendley@gmail.com>
2021-08-09 17:13:29 -0400

    Remove certificate and biography section

--- app/src/main/java/com/android/leybiography/MainActivity.java
+++ app/src/main/java/com/android/leybiography/MainActivity.java
@@ -1,16 +1,15 @@
 package com.android.leybiography;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -67,17 +66,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                         new ProfileFragment()).commit();
                 break;
-            case R.id.nav_biography:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        new BiographyFragment()).commit();
-                break;
             case R.id.nav_language_learned:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                         new LanguageFragment()).commit();
-                break;
-            case R.id.nav_certificate:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        new CertificateFragment()).commit();
                 break;
             case R.id.nav_share:
                 Intent shareApp = new Intent();
